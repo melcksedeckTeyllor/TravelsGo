288457beec130aac11f6fdbf3d8656f7d1859a25
MelckTeyllor <melcksedeck123@gmail.com>
2024-11-02 09:48:30 -0300

    create project

---
+++ Js/menu.js
@@ -0,0 +1,30 @@
+let navBar = document.querySelector('#header');
+
+document.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+    if( scrollTop > 0 ){
+        navBar.classList.add('rolar')
+    }else {
+        navBar.classList.remove('rolar')
+    }
+})
+
+
+//Menu Mob
+
+let btnMenu = document.querySelector('#btn-menu-mob');
+let line1 = document.querySelector('.line-menu-mob-1');
+let line2 = document.querySelector('.line-menu-mob-2');
+let menuMobile = document.querySelector('#menu-mobile');
+
+let body = document.querySelector('body')
+
+btnMenu.addEventListener('click', () => {
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+
+    menuMobile.classList.toggle('abrir')
+
+    body.classList.toggle('no-overflow')
+})
+
